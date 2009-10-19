79a629e6e5da5710784fadcec4b6a2a300dd2a96
Julio Javier Cicchelli <me@javiercicchelli.info>
2009-10-19 23:50:50 +0200

    Added the JSON conversion for both DataMapper::Resource and DataMapper::Collection.

--- lib/rubyists/model.rb
+++ lib/rubyists/model.rb
@@ -3,27 +3,27 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), 'data_model')
 require 'dm-core'
 require 'dm-validations'
 require 'dm-aggregates'
-#require 'json'
+require 'json'
 
 PATTERN_CODE = /^[A-Z]{2}$/
 PATTERN_NAME = /^([A-Z][a-z\.\-]*\s?)+$/
 PATTERN_EMAIL = /^(?:[a-z]+)(\.[\w\-]+)*@([\w\-]+)(\.[\w\-\.]+)*(\.[a-z]{2,4})$/i
 PATTERN_URL = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/\.*)?$/ix
 
-#module DataMapper
-#  module Resource
-#    def to_json
-#      attributes.to_json
-#    end
-#  end
-#  
-#  class Collection
-#    def to_json
-#      to_a.to_json
-#    end
-#  end
-#end
-#
+module DataMapper
+  module Resource
+    def to_json
+      attributes.to_json
+    end
+  end
+  
+  class Collection
+    def to_json
+      to_a.to_json
+    end
+  end
+end
+
 #module Rubyists
 #  module Resource
 #    def destroy_all
