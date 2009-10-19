15f328b534959e9ab4671bdcf1b0d51745f211c6
Julio Javier Cicchelli <me@javiercicchelli.info>
2009-10-19 23:54:16 +0200

    jQuerified the application javascript in an unobstructive way.

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -3,51 +3,6 @@ var geocoder = null;
 var base_url = null;
 
 /*
-function each(ary, block) {
-  for (i in ary) {
-    block(ary[i]);
-  }
-}
-
-function findAddress(address, callback) {
-  if (geocoder) {
-    var callback = callback;
-    geocoder.getLatLng(
-      address,
-      function(point) {
-        if (!point) {
-          alert(address + " not found");
-        } else {
-          callback(point);
-        }
-      }
-    );
-  }
-}
-
-function placeMarker(point, text) {
-  var text = text;
-  var marker = new GMarker(point);
-  map.addOverlay(marker);
-  GEvent.addListener(marker, "click", function() {
-    marker.openInfoWindowHtml(text);
-  });
-  return marker;
-}
-
-function tag(name, content) {
-  return "<" + name + ">" + content + "</" + name + ">"
-}
-
-function addUserGroupMarker(name, city, country) {
-  var name = name;
-  var address = city + ", " + country
-  findAddress(address, function(point) {
-    html = tag('h3', name) + tag('p', address)
-    placeMarker(point, html);
-  })
-} 
-
 function showAddress(address) {
   var address = address;
   findAddress(address, function(point) {
@@ -68,11 +23,49 @@ jQuery.googleMaps = {
 		    map.setCenter(new GLatLng(54.0, -6.24), 4);
 		    map.addControl(mapControl);
 		}
+	}, 
+	countryOf: function(code) {
+		var country = null;
+
+		$.ajax({url: '/countries/' + code, async: false, dataType: 'json', success: function(json) {country = json.name;}});
+
+		return country;
+	},
+	findAddress: function(location, callback) {
+		if (geocoder) {
+	    	geocoder.getLatLng(location, function(point) {
+				if (!point) {alert(location + " not found");} 
+				else {callback(point);}
+	      	});
+	  	}
+	},
+	placeMarker: function(point, text) {
+	  	var text = text;
+	  	var marker = new GMarker(point);
+	  
+		map.addOverlay(marker);
+	  	GEvent.addListener(marker, "click", function() {marker.openInfoWindowHtml(text);});
+		
+		return marker;
+	},
+	markGroups: function() {
+		$.getJSON('/groups', function(groups) {
+			$.each(groups, function() {
+				var group = this;
+				var country = $.googleMaps.countryOf(group.country_code);
+				var location = group.city + ', ' + country;
+				
+				$.googleMaps.findAddress(location, function(point) {
+					html = '<a href="' + group.website + '"><h3>' + group.name + '</h3></a><p>' + location + '</p>';
+			    	$.googleMaps.placeMarker(point, html);
+				});				
+			});
+		});
 	}
 }
 
 jQuery.fn.openLink = function(url) {
-	$(this).click(function(){window.open(url, '_self');});
+	$(this).click(function() {window.open(url, '_self');});
 }
 
 jQuery.goTo = function(url) {
@@ -84,7 +77,8 @@ jQuery.goTo = function(url) {
 }
 
 $(document).ready(function() {
-	$(window).load(function(){$.googleMaps.initialize();});
+	$.googleMaps.initialize();
+	$.googleMaps.markGroups();
 	// Header.
 	$('.about').click(function(){
 		$('#dialog').css({'width': '700px', 'height': '500px'});
@@ -93,19 +87,19 @@ $(document).ready(function() {
 			$('#dialog > .text').load('/about');
 		});
 	});
-	$('.groups').click(function(){$.goTo('/groups');});
+	$('.groups').click(function() {$.goTo('/groups');});
 	//$('li.search').click();
-	$('.title').click(function(){$.goTo('/')});
+	$('.title').click(function() {$.goTo('/')});
 	// Dialog.
-	$('#dialog > .close').click(function(){
-		$('#dialog').fadeOut('slow', function(){
+	$('#dialog > .close').click(function() {
+		$('#dialog').fadeOut('slow', function() {
 			$('#map, #content').css({'opacity': '1.0'});
 			$('#dialog > .text').html("");
 		});
 	});
-	$('.actions > ul > .add').click(function(){
+	$('.actions > ul > .add').click(function() {
 		$('#dialog').css({'width': '500px', 'height': '300px'});
-		$('#dialog').fadeIn('slow', function(){
+		$('#dialog').fadeIn('slow', function() {
 			$('#map, #content').css({'opacity': '0.65'});
 			$('#dialog > .text').load('/groups/new');
 		});
